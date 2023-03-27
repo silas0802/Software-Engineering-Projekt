f7aa728c6be8d12602cc97cc615cf168b4ff8819
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2023-03-27 13:21:37 +0200

    User Class

--- Java Projekt/src/main/User.java
+++ Java Projekt/src/main/User.java
@@ -1,4 +1,46 @@
 package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    String userName;
+    String timeWorked;
+    List<Activity> activities = new ArrayList<Activity>();
+
+
+
+
+    public String user (String userName){
+
+        this.userName = userName;
+
+
+        return this.userName;
+    }
+
+
+
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getTimeWorked() {
+        return timeWorked;
+    }
+    public void setTimeWorked(String timeWorked) {
+        this.timeWorked = timeWorked;
+    }
+
+
+    void registerTimeWorked(Activity active, int workingHours){
+        // ToDO activity.workload(int)
+        activities.add(active);
+        timeWorked += workingHours;
+    }
+
+
     
 }

45c76d49602146c5ed19a80c794988e0dde23425
jesperbesper <jesper.p03@gmail.com>
2023-03-27 13:26:27 +0200

    Ativity

--- Java Projekt/src/main/Activity.java
+++ Java Projekt/src/main/Activity.java
@@ -1,4 +1,55 @@
 package main;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Activity {
     
+    GregorianCalendar startTime;
+    GregorianCalendar endTime;
+    String name;
+    int expectedDuration;
+    public GregorianCalendar getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(GregorianCalendar startTime) {
+        this.startTime = startTime;
+    }
+
+    public GregorianCalendar getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(GregorianCalendar endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getExpectedDuration() {
+        return expectedDuration;
+    }
+
+    public void setExpectedDuration(int expectedDuration) {
+        this.expectedDuration = expectedDuration;
+    }
+
+    public String getDescribtion() {
+        return describtion;
+    }
+
+    public void setDescribtion(String describtion) {
+        this.describtion = describtion;
+    }
+
+    String describtion; 
+
+    public Activity(String name){
+        this.name=name;
+        
+
+    }   
+
+
 }
+
+

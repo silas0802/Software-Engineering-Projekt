3440663738c0bf0cd5f9668d1ffcc50d2e430e51
silas0802 <40304986+silas0802@users.noreply.github.com>
2023-03-27 13:30:24 +0200

    Update ProjectManagerApp.java

--- Java Projekt/src/main/ProjectManagerApp.java
+++ Java Projekt/src/main/ProjectManagerApp.java
@@ -1,5 +1,18 @@
 package main;
 
+import java.util.ArrayList;
+
 public class ProjectManagerApp {
-    
+    ArrayList<User> users = new ArrayList<>();
+    ArrayList<Project> projects = new ArrayList<>();
+
+    public void createUser(User user){
+        users.add(user);
+    }
+    public void createProject(Project project){
+        projects.add(project);
+    }
+    public void createActivity(Project project, Activity activity){
+        project.setActivity(activity);
+    }
 }

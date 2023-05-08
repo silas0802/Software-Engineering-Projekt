eb971dbad009abc0394536d67d97e4b6755c6e8d
silas0802 <40304986+silas0802@users.noreply.github.com>
2023-05-08 12:42:18 +0200

    workedlisttest

---
+++ Java Project/features/WorkerTimeList.feature
@@ -0,0 +1,20 @@
+Feature: WorkerTimeList
+Description: manipulating WorkerTimeLists
+Actors: User
+
+#Silas Thule
+Background: a user and WorkerTimeList
+Given a WorkerTimeList exists
+And a User exists
+
+Scenario: User gets workedTimeList
+Given the user registers 10.0 hours in WorkerTimeList
+When the user gets workedTimeList
+Then workedTimeList contains user with 10.0 hours
+
+Scenario: User checks totaltime spent
+Given the user registers 10.0 hours in WorkerTimeList
+And a User exists
+Given the user registers 17.0 hours in WorkerTimeList
+When the user gets totalTimeWorked
+Then 27.0 is returned
--- Java Project/src/test/java/example/cucumber/Hourssteps.java
+++ Java Project/src/test/java/example/cucumber/Hourssteps.java
@@ -4,11 +4,15 @@ import static org.junit.Assert.assertTrue;
 
 import application.OperationNotAllowedException;
 import application.ProjectManagerApp;
+import application.User;
+import application.WorkerTime;
+import application.WorkerTimeList;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 /**
- * @author Niclas
+ * @author Niclas, Silas Thule
  */
 public class Hourssteps {
 
@@ -33,5 +37,53 @@ public class Hourssteps {
         // Write code here that turns the phrase above into concrete actions
         assertTrue(VariablesHolder.user.getTimeWorked()==VariablesHolder.workHours);
         assertTrue(VariablesHolder.activity.getWorkedTime()==VariablesHolder.workHours);
+    }
+    //Silas Thule
+    @Given("a WorkerTimeList exists")
+    public void aWorkerTimeListExists() {
+        // Write code here that turns the phrase above into concrete actions
+        VariablesHolder.workerTimeList = new WorkerTimeList();
+    }
+    //Silas Thule
+    @Given("a User exists")
+    public void aUserExists() {
+        // Write code here that turns the phrase above into concrete actions
+        VariablesHolder.user = new User("Torb");
+    }
+    //Silas Thule
+    @Given("the user registers {double} hours in WorkerTimeList")
+    public void theUserRegistersHoursInWorkerTimeList(Double int1) {
+        // Write code here that turns the phrase above into concrete actions
+        VariablesHolder.workerTimeList.registerTime(VariablesHolder.user, int1);
+    }
+    //Silas Thule
+    @When("the user gets workedTimeList")
+    public void theUserGetsWorkedTimeList() {
+        // Write code here that turns the phrase above into concrete actions
+        VariablesHolder.workerTimes = VariablesHolder.workerTimeList.getWorkedTimeList();
+    }
+    //Silas Thule
+    @Then("workedTimeList contains user with {double} hours")
+    public void workedTimeListContainsUserWithHours(Double int1) {
+        // Write code here that turns the phrase above into concrete actions
+        boolean check = false;
+        for (WorkerTime worktime : VariablesHolder.workerTimes) {
+            if (worktime.user == VariablesHolder.user && worktime.time == int1) {
+                check = true;
+            }
+        }
+        assertTrue(check);
+    }
+    //Silas Thule
+    @When("the user gets totalTimeWorked")
+    public void theUserGetsTotalTimeWorked() {
+        // Write code here that turns the phrase above into concrete actions
+        VariablesHolder.workHours = VariablesHolder.workerTimeList.totalTimeWorked();
+    }
+    //Silas Thule
+    @Then("{double} is returned")
+    public void isReturned(Double double1) {
+        // Write code here that turns the phrase above into concrete actions
+        assertTrue(VariablesHolder.workHours == double1);
     }
 }
